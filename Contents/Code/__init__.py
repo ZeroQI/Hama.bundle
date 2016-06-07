74fc38eea89a23a8fd3792c649a6d4a1f0a41e1b
ZeroQI <benjamin.brisson@gmail.com>
2016-06-07 23:55:30 +0100

    Metadata.theme missing log add
    https://github.com/ZeroQI/Hama.bundle/issues/49
    
    Old code had it: https://raw.githubusercontent.com/ZeroQI/Hama.bundle/60ec132c27733ca4b86ebb41f0f13b1fa14688c5/Contents/Code/__init__.py
    
    error_log   ['Plex themes missing'      ].append("anidbid: %s, title: '%s', tvdbid: %s, title: '%s' <a href='mailto:themes@plexapp.com?cc=&subject=Missing%%20theme%%20song%%20-%%20&#39;%s%%20-%%20%s.mp3&#39;'>Upload</a>" % (metadata.id.zfill(5), orig, tvdbid.zfill(5), tvdb_title, tvdb_title, tvdbid) )

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -264,7 +264,9 @@ class HamaCommonAgent:
 
       ### Plex - Plex Theme song - https://plexapp.zendesk.com/hc/en-us/articles/201178657-Current-TV-Themes ###
       if THEME_URL % tvdbid in metadata.themes:  Log.Debug("Update() - Theme song - already added")
-      else:                                      self.metadata_download (metadata.themes, THEME_URL % tvdbid, 1, "Plex/"+metadata.id+".mp3")  #if local, load it ?
+      else:
+        self.metadata_download (metadata.themes, THEME_URL % tvdbid, 1, "Plex/"+metadata.id+".mp3")  #if local, load it ?
+        if not THEME_URL % tvdbid in metadata.themes:  error_log['Plex themes missing'].append("anidbid: %s, title: '%s', tvdbid: %s, title: '%s' <a href='mailto:themes@plexapp.com?cc=&subject=Missing%%20theme%%20song%%20-%%20&#39;%s%%20-%%20%s.mp3&#39;'>Upload</a>" % (metadata.id.zfill(5), orig, tvdbid.zfill(5), tvdb_title, tvdb_title, tvdbid) )
       
       ### TVDB - Load serie XML ###
       tvdbanime, tvdb_episode_missing, tvdb_special_missing, special_summary_missing, summary_missing, summary_present = None, [], [], [], [], []
