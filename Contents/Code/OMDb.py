7947c3f6b3ad09a240c9363a3a9fac080638700c
ZeroQI <benjamin.brisson@gmail.com>
2017-01-25 17:42:51 +0000

    Update OMDb.py

--- Contents/Code/OMDb.py
+++ Contents/Code/OMDb.py
@@ -7,8 +7,8 @@ def omdb_poster(metadata, imdbid_multiple, num=98):
     Log.Info("OMDB - background, Poster - imdbid: '%s'" % imdbid)
     for imdbid in imdbid_multiple.split(","):
       Log.Info("imdbid: '%s', url: '%s', filename: '%s'" % (imdbid, OMDB_HTTP_API_URL + imdbid, "OMDB/%s.jpg" % imdbid))
-      try:                   OMDB = common.get_json(OMDB_HTTP_API_URL + imdbid, cache_time=CACHE_1WEEK * 56)
-      except Exception as e: Log.Error("Exception - imdbid: '%s', url: '%s', filename: '%s', Exception: '%s'" % (imdbid, OMDB_HTTP_API_URL + imdbid, "OMDB/%s.jpg" % imdbid, e))
+      try:                    OMDB = common.get_json(OMDB_HTTP_API_URL + imdbid, cache_time=CACHE_1WEEK * 56)
+      except Exception as e:  Log.Error("Exception - imdbid: '%s', url: '%s', filename: '%s', Exception: '%s'" % (imdbid, OMDB_HTTP_API_URL + imdbid, "OMDB/%s.jpg" % imdbid, e))
       else:
         if OMDB and 'Poster' in OMDB and OMDB['Poster'] not in ("N/A", "", None):  common.metadata_download (metadata.posters, OMDB['Poster'], num, "OMDB/%s.jpg" % imdbid)
         else:                                                                      Log.Info("No poster to download - " + OMDB_HTTP_API_URL + imdbid)
