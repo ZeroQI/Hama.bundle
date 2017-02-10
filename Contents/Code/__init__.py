66d8521a3c9071e9c98901e511a986907bd8cdb6
ZeroQI <benjamin.brisson@gmail.com>
2017-02-10 17:42:11 +0000

    Update __init__.py

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,80 +1,96 @@
 # -*- coding: utf-8 -*-
-import common          # HAMA generic   functions: getImagesFromASS, write_logs
-import AnimeLists      # AnimeLists     functions: anidbTvdbMapping
-import AniDB           # AniDB     .net functions: Search_AniDB, 
-import tvdb            # TheTVDB   .com functions: Search_TVDB, get_tvdb_metadata, tvdb_update_meta, plex_theme_song, getImagesFromTVDB
-import tmdb            # TheMovieDB.net functions: Search_TMDB, TMDB_Tagline_Trailers, Update_TMDB
-import FanartTv        # FanartTV  .com functions: fanarttv_posters, get_tmdbid_per_imdbid
-import OMDb            # OMDbAPI   .com functions: omdb_poster
+# https://www.python.org/dev/peps/pep-0008/
 
-### Pre-Defined Start function #########################################################################################################################################
-def Start():
-  import logging
-  for handler in logging.getLogger('com.plexapp.agents.hama').handlers:  handler.setFormatter(logging.Formatter('%(asctime)-15s - %(name)s (%(thread)x) : %(levelname)s (%(module)s/%(funcName)s:%(lineno)d) - %(message)s'))
-  Log.Info("HTTP Anidb Metadata Agent (HAMA)v By ZeroQI (Forked from Atomicstrawberry's v0.4 - AniDB, TVDB mod agent using SdudLee XML's ###")
-  HTTP.CacheTime = CACHE_1HOUR * 24 * 2  
+### Imports ###
+import time        # sleep, strftime
+import common      # Functions: Logging, SaveFile, LoadFile, metadata_download, WriteLogs, cleanse_title, getImagesFromASS, GetMeta 
+                   # Functions: (Direct import) GetElementText, GetPosters, GetSeasons, 
+                   # Variables: Too many...
+import AnimeLists  # Functions: GetAniDBTVDBMap, GetAniDBMovieSet, anidbTvdbMapping, MergeAniDBTVDBMaps  Variables: MAPPING_FEEDBACK
+import AniDB       # Functions: GetMetadata, GetAniDBTitlesDB, GetAniDBTitle, Search                     Variables: ANIDB_SERIE_URL, ANIDB_PIC_BASE_URL
+import TheTVDB     # Functions: GetImages, Search, LoadMetadata                                          Variables: TVDB_SERIE_URL
+import TheMovieDB  # Functions: GetImages, Search, Tagline_Trailers, get_TMDbid_per_IMDbid               Variables: TVDB_SERIE_URL
+import FanartTV    # Functions: GetImages                                                                Variables: None
+import MyAnimeList # Functions: GetImages                                                                Variables: None
+import OMDb        # Functions: GetImages                                                                Variables: None
+import Plex        # Functions: GetThemes                                                                Variables: None
+
+### Variables ###
+AniDBTitlesDB  = None
+AniDBMovieSets = None
+AniDBTVDBMap   = None
 
 ### Pre-Defined ValidatePrefs function Values in "DefaultPrefs.json", accessible in Settings>Tab:Plex Media Server>Sidebar:Agents>Tab:Movies/TV Shows>Tab:HamaTV #######
 def ValidatePrefs():
-  Log.Info( "ValidatePrefs()")
-  DefaultPrefs = ("GetTvdbFanart", "GetTvdbPosters", "GetTvdbBanners", "GetAnidbPoster", "GetTmdbFanart", "GetTmdbPoster", "GetOmdbPoster", "GetFanartTVBackground", "GetFanartTVPoster", "GetFanartTVBanner", "GetASSPosters", "localart", "adult", 
-                  "GetPlexThemes", "MinimumWeight", "SerieLanguage1", "SerieLanguage2", "SerieLanguage3", "EpisodeLanguage1", "EpisodeLanguage2")
+  Log.Info ("ValidatePrefs()")
+  DefaultPrefs = ("GetSingleOne", "Posters", "Seasons", "Fanarts", "Banners", "Themes", "TheTVDB", "AniDB", "TMDb", "Omdb", "FanartTV", "MyAnimeList", "ASSPosters", 
+                  "localart", "adult", "MinimumWeight", "SerieLanguage1", "SerieLanguage2", "SerieLanguage3", "EpisodeLanguage1", "EpisodeLanguage2")
   try:  
-    for key in DefaultPrefs: Log.Info("Prefs[{key:<{width}}] = {value}".format(key=key, width=max(map(len, DefaultPrefs)), value=Prefs[key]))
-    if [Prefs[key] == None for key in DefaultPrefs].count(True) > 0: Log.Error("Some Pref values do not exist. Edit and save your preferences.")
-  except:  Log.Error("DefaultPrefs.json invalid, Value '%s' missing, update it and save." % key);  return MessageContainer ('Error', "Value '%s' missing from 'DefaultPrefs.json'" % key)
-  else:    Log.Info( "DefaultPrefs.json is valid, Provided preference values are ok");             return MessageContainer ('Success', "DefaultPrefs.json valid")
+    for key in DefaultPrefs:
+      Log.Info("Prefs[{key:<{width}}] = {value}".format(key=key, width=max(map(len, DefaultPrefs)), value=Prefs[key]))
+      if Prefs[key] == None:  Log.Error("Some Pref values do not exist. Edit and save your preferences.")
+  except:  Log.Error("ValidatePrefs() - DefaultPrefs.json, Value '%s' missing, update it and save." % key);  return MessageContainer ('Error', "Value '%s' missing from 'DefaultPrefs.json'" % key)
+  else:    Log.Info( "ValidatePrefs() - DefaultPrefs.json valid");                                           return MessageContainer ('Success', "DefaultPrefs.json valid")
+
+### Pre-Defined Start function ##################################################################################################################################################
+def Start():
+  common.Logging()
+  Log.Info("".ljust(157, '='))
+  Log.Info("HTTP Anidb Metadata Agent (HAMA)v By ZeroQI (Forked from Atomicstrawberry's v0.4 - AniDB, TVDB mod agent using SdudLee XML's ###")
+  Log.Info("".ljust(157, '='))
   
-### Movie/Serie search #############################################################################################################################
+  HTTP.CacheTime                   = CACHE_1DAY  # in sec - was (=3600): CACHE_1HOUR
+  HTTP.Headers['User-Agent'      ] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:22.0) Gecko/20100101 Firefox/22.0'
+  HTTP.Headers['X-Requested-With'] = 'XMLHttpRequest'
+
+  global AniDBTitlesDB ;  AniDBTitlesDB  =      AniDB.GetAniDBTitlesDB()
+  global AniDBMovieSets;  AniDBMovieSets = AnimeLists.GetAniDBMovieSets()
+  global AniDBTVDBMap  ;  AniDBTVDBMap   = AnimeLists.GetAniDBTVDBMap()
+  Log.Info("".ljust(157, '='))
+  #msgContainer = ValidatePrefs();  if msgContainer.header == 'Error': return
+  
+### Movie/Serie search ##########################################################################################################################################################
 def Search(results, media, lang, manual, movie):
   Log.Info('--- Search Begin -------------------------------------------------------------------------------------------')
   if movie and media.title == "clear-cache" or not movie and media.show == "clear-cache":  HTTP.ClearCache()  ### Clear Plex http cache manually by searching a serie named "clear-cache" ###
-  maxi = AniDB.Search_AniDB(results, media, lang, manual, movie)
-  #if maxi<50:  maxi = tvdb.Search_TVDB(results, media, lang, manual, movie)
-  #if maxi<50:  tmdb.Search_TMDB(results, media, lang, manual, movie)
-    
-### Update Movie/Serie from metadata.id assigned ###################################################################################################
+  maxi = AniDB.Search(results, media, lang, manual, movie, AniDBTitlesDB)
+  
+### Update Movie/Serie from metadata.id assigned ################################################################################################################################
 def Update(metadata, media, lang, force, movie):
-  Log.Info('--- Update Begin -------------------------------------------------------------------------------------------')
-  import time # sleep, strftime
-
+  
   ### Variables initialisation ###
-  anidbid, tvdbid, tmdbid, imdbid, mappingList, tvdb_table, current_date = "", "", "", "", {}, {}, int(time.strftime("%Y%m%d"))
-  error_log = { 'anime-list anidbid missing':[], 'anime-list tvdbid missing'  :[], 'anime-list studio logos'  :[], 'Plex themes missing'      :[],
-                'AniDB summaries missing'   :[], 'AniDB posters missing'      :[], 'Missing Episodes'         :[], 'Missing Specials'         :[], 
-                'TVDB posters missing'      :[], 'TVDB season posters missing':[], 'Missing Episode Summaries':[], 'Missing Special Summaries':[]}
+  error_log = { 'AniDB summaries missing'   :[], 'AniDB posters missing'      :[], 'Missing Episodes'         :[], 'Missing Specials'         :[], 'anime-list AniDBid missing':[], 'anime-list TVDBid missing':[], 
+                'TVDB posters missing'      :[], 'TVDB season posters missing':[], 'Missing Episode Summaries':[], 'Missing Special Summaries':[], 'anime-list studio logos'   :[], 'Plex themes missing'      :[]}
+  AniDBid, TVDBid, TMDbid, IMDbid, ANNid, MALid, mappingList, TheTVDB_dict, current_date           = "", "", "", "", "", "", {}, {}, int(time.strftime("%Y%m%d"))
+  AniDB_dict, MyAnimeList_dict, FanartTV_dict, OMDb_dict, TheTVDB_dict, TheMovieDB_dict, Plex_dict = {}, {}, {}, {}, {}, {}, {}
   
   ### Metadata source/id separation and finding other metadata ids ###
-  metadata_source, metadata_id = metadata.id.split('-', 1)
-  Log.Info("metadata source: '%s', id: '%s', Title: '%s', lang: '%s', force: '%s', movie: '%s'" % (metadata_source, metadata_id, metadata.title, lang, force, movie))
-  if metadata_source.startswith("anidb"):
-    anidbid = metadata_id
-    if anidbid.isdigit():  tvdbid, tmdbid, imdbid, mappingList = AnimeLists.anidbTvdbMapping(metadata, media, movie, anidbid, error_log)  
-  elif metadata_source.startswith("tvdb"):  tvdbid = metadata_id_number
-  #elif metadata_source.startswith("tmdb"):  tmdbid = metadata_id_number
-  #elif metadata_source.startswith("tsdb"):  tsdbid = metadata_id_number
-  if tvdbid.isdigit():  tvdb_table = tvdb.get_tvdb_metadata(metadata, media, lang, movie, metadata_source, tvdbid, imdbid, mappingList['defaulttvdbseason'], error_log)         # Updates imdbid from tvdbid, used by omdb
+  Log.Info('--- Update Begin -------------------------------------------------------------------------------------------')
+  Log.Info("Update() - metadata.id: '%s', Title: '%s', lang: '%s', force: '%s', movie: '%s', max(media.seasons.keys()): '%s'" % (metadata.id, metadata.title, lang, force, movie, str(max(int(x) for x in media.seasons.keys())>1)))
+  source, id = metadata.id.split('-', 1)
+  if   source.startswith("tvdb" ) and id.isdigit():  TVDBid  = id; 
+  elif source.startswith("anidb") and id.isdigit():
+    AniDBid = id
+    TVDBid, TMDbid, IMDbid, mappingList  = AnimeLists.anidbTvdbMapping(metadata, media, movie, AniDBid,                                      error_log, AniDBTVDBMap)
+    ANNid,  MALid,          AniDB_dict   = AniDB.GetMetadata          (metadata, media, movie, AniDBid, TVDBid, mappingList, AniDBMovieSets, error_log, current_date)
   
-  ### Recover Backgrounds, posters, theme song ###
-  if (tvdbid.isdigit()                    ) and (Prefs['GetPlexThemes'    ]                                  ):  tvdb.plex_theme_song      (metadata, tvdbid, tvdb_table, error_log)                           ### Plex - Plex Theme song - https://plexapp.zendesk.com/hc/en-us/articles/201178657-Current-TV-Themes ###
-  if (tvdbid.isdigit()                    ) and (Prefs['GetTvdbPosters'   ] or Prefs['GetTvdbFanart'        ]):  tvdb.getImagesFromTVDB    (metadata, media, error_log, tvdbid, tvdb_table, movie, force, mappingList['defaulttvdbseason'], 1)
-  if (imdbid.isdigit()                    ) and (Prefs['GetOmdbPoster'    ]                                  ):  omdb.omdb_poster          (metadata, imdbid)                                                 ### OMDB - Posters - Using imdbid ###  return 200 but not downloaded correctly - IMDB has a single poster, downloading through OMDB xml, prefered by mapping file
-  if (imdbid.isdigit() or tmdbid.isdigit()) and (Prefs['GetTmdbPoster'    ] or Prefs['GetTmdbFanart'        ]):  tmdb.tmdb_posters         (metadata, imdbid, tmdbid)                                         ### - TMDB - background, Poster - using imdbid or tmdbid ### The Movie Database is least prefered by the mapping file, only when imdbid missing
-  if (imdbid.isdigit() or tmdbid.isdigit()) and (Prefs['GetFanartTVPoster'] or Prefs['GetFanartTVBackground']):  FanartTv.fanarttv_posters (metadata, movie, tmdbid, tvdbid, imdbid, mappingList)       ### fanart.tv - Background, Poster and Banner - Using imdbid ###
-  if (tmdbid.isdigit()                    ) and (movie                                                       ):  tmdb.TMDB_Tagline_Trailers(metadata, movie, lang, tmdbid, imdbid)                            ### Populate Movie Metadata Extras (e.g. Taglines) from TMDB for Movies ###
-  if (metadata_source == "tvdb4"          ) and (Prefs['GetASSPosters']                                      ):  common.getImagesFromASS   (metadata, media, tvdbid, movie, 0)                                ### ASS tvdb4 ark posters ###
+  if  TVDBid.isdigit():                              TheTVDB_dict = TheTVDB.GetMetadata (metadata, media, movie, lang, source, TVDBid, IMDbid, mappingList['defaulttvdbseason'] if 'defaulttvdbseason' in mappingList else "", error_log, current_date)  # Updates IMDbid from TVDBid, used by omdb
+  if  TVDBid.isdigit():                                 Plex_Dict =    Plex.GetMetadata (metadata, TVDBid, TheTVDB_dict, error_log)                                                            ### Plex - Plex Theme song - https://plexapp.zendesk.com/hc/en-us/articles/201178657-Current-TV-Themes ###  # Prefs['Themes'  ]
+  if  IMDbid.startswith("tt"):                          OMDb_dict =    OMDb.GetMetadata (metadata, IMDbid, movie, mappingList['name'])                                                                    ### OMDB - Posters - Using IMDbid ###  return 200 but not downloaded correctly - IMDB has a single poster, downloading through OMDB xml, prefered by mapping file
+  #if  TMDbid.isdigit() and movie:                       TMDb.TaglineTrailers (metadata, movie, lang, TMDbid, IMDbid)                                                               ### Populate Movie Metadata Extras (e.g. Taglines) from TMDB for Movies ###
+  #if  TVDBid.isdigit():                              TheTVDB.GetImages       (metadata, media, error_log, TVDBid, TheTVDB_dict, movie, force, mappingList['defaulttvdbseason'], 1)  #if Prefs['TheTVDB' ] and (GetPosters() or GetSeasons() or GetFanarts() or GetBanners()):
+  #if  IMDbid.startswith("tt") or TMDbid.isdigit():      TMDb.GetImages       (metadata, IMDbid, TMDbid)                                                                            ### - TMDB - background, Poster - using IMDbid or TMDbid ### The Movie Database is least prefered by the mapping file, only when IMDbid missing
+  #if  IMDbid.startswith("tt") or TMDbid.isdigit():  FanartTV.GetImages       (metadata, TVDBid, IMDbid, TMDbid, movie)                                             ### fanart.tv - Background, Poster and Banner - Using IMDbid ###
+  #if   MALid.isdigit():                          MyAnimeList.GetImages       (metadata, MALid)
+  #if source == "tvdb4":                               common.getImages       (metadata, media, TVDBid, movie, 0)                                                                   ### ASS tvdb4 ark posters ###
   
   ### Update other metadata ###
-  if not movie and tvdbid.isdigit() and (max(int(x) for x in media.seasons.keys())>1 or metadata_source.startswith("tvdb")):  
-    Log.Info("meta source: '%s', max(media.seasons.keys()): '%s', media.seasons.keys(): '%s'" % (str(metadata_source.startswith("tvdb")), str(max(int(x) for x in media.seasons.keys())>1), str(media.seasons.keys())))
-    tvdb.tvdb_update_meta     (metadata, media, metadata_source, mappingList['defaulttvdbseason'], tvdb_table)  ### TVDB mode when a season 2 or more exist ###
-  elif metadata_source.startswith("anidb"):                                                                       AniDB.anidb_update_meta   (metadata, media, movie, anidbid, tvdbid, tvdb_table, mappingList, AnimeLists.AniDB_collection_tree, error_log, current_date)
-  #elif metadata_source.startswith("tmdb" ):                                                                      tmdb.Update_TMDB          (metadata, media, lang, force, movie)
-  #elif metadata_source.startswith("tsdb" ):                                                                      tmdb.Update_TMDB          (metadata, media, lang, force, movie)
-  common.write_logs(media, movie, error_log, metadata_source, metadata_id, anidbid, tvdbid)
-  Log.Info('--- Update end -------------------------------------------------------------------------------------------------')
+  MetaSources = {'AniDB': AniDB_dict, 'MyAnimeList': MyAnimeList_dict, 'FanartTV': FanartTV_dict, 'OMDb': OMDb_dict, 'TheTVDB': TheTVDB_dict, 'TheMovieDB':   TheMovieDB_dict, 'Plex':         Plex_dict}
+  common.UpdateMeta(metadata, MetaSources, movie)
+  common.write_logs(media, movie, error_log, source, source, AniDBid, TVDBid)
+Log.Info('--- Update end -------------------------------------------------------------------------------------------------')
 
-### Agent declaration ###############################################################################################################################################
+### Agent declaration ###########################################################################################################################################################
 class HamaTVAgent(Agent.TV_Shows):
   name             = 'HamaTV'
   primary_provider = True
@@ -82,8 +98,8 @@ class HamaTVAgent(Agent.TV_Shows):
   contributes_to   = None
   accepts_from     = ['com.plexapp.agents.localmedia', 'com.plexapp.agents.none'] # 'com.plexapp.agents.opensubtitles'
   languages        = [Locale.Language.English, 'fr', 'zh', 'sv', 'no', 'da', 'fi', 'nl', 'de', 'it', 'es', 'pl', 'hu', 'el', 'tr', 'ru', 'he', 'ja', 'pt', 'cs', 'ko', 'sl', 'hr']
-  def search(self, results,  media, lang, manual):  Search(results,  media, lang, manual, False )
-  def update(self, metadata, media, lang, force ):  Update(metadata, media, lang, force,  False )
+  def search (self, results,  media, lang, manual):  Search (results,  media, lang, manual, False)
+  def update (self, metadata, media, lang, force ):  Update (metadata, media, lang, force,  False)
 
 class HamaMovieAgent(Agent.Movies):
   name             = 'HamaMovies'
@@ -92,5 +108,5 @@ class HamaMovieAgent(Agent.Movies):
   contributes_to   = None
   languages        = [Locale.Language.English, 'fr', 'zh', 'sv', 'no', 'da', 'fi', 'nl', 'de', 'it', 'es', 'pl', 'hu', 'el', 'tr', 'ru', 'he', 'ja', 'pt', 'cs', 'ko', 'sl', 'hr']
   accepts_from     = ['com.plexapp.agents.localmedia', 'com.plexapp.agents.none'] # 'com.plexapp.agents.opensubtitles'
-  def search(self, results,  media, lang, manual):  Search(results,  media, lang, manual, True )
-  def update(self, metadata, media, lang, force ):  Update(metadata, media, lang, force,  True )
+  def search (self, results,  media, lang, manual):  Search (results,  media, lang, manual, True)
+  def update (self, metadata, media, lang, force ):  Update (metadata, media, lang, force,  True)
