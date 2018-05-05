ac427de476a692d7dc759afa2a5ca9a006aaf5d3
ZeroQI <benjamin.brisson@gmail.com>
2018-05-05 01:05:26 +0100

    on empy tvdb actor info: 'NoneType' object has no attribute 'iter'
    2018-05-04 15:27:23,255 (6f2ac54da700) : INFO (TheTVDBv2:170) - TheTVDB.GetMetadata() - TVDB_ACTORS_URL: https://api.thetvdb.com/series/326337/actors, actor_json: None
    2018-05-04 15:27:23,255 (6f2ac54da700) : CRITICAL (agentkit:1078) - Exception in the update function of agent named 'HamaTV', called with guid 'com.plexapp.agents.hama://anidb-12483?lang=en' (most recent call last):
    File "/usr/lib/plexmediaserver/Resources/Plug-ins-215c28d86/Framework.bundle/Contents/Resources/Versions/2/Python/Framework/api/agentkit.py", line 1076, in _update
    agent.update(obj, media, lang, **kwargs)
    File "/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-ins/Hama.bundle/Contents/Code/init.py", line 125, in update
    def update (self, metadata, media, lang, force ): Update (metadata, media, lang, force, False)
    File "/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-ins/Hama.bundle/Contents/Code/init.py", line 103, in Update
    dict_TheTVDB, IMDbid = TheTVDBv2.GetMetadata(media, movie, error_log, lang, source, AniDBid, TVDBid, IMDbid, mappingList, Dict(AniDB, 'movie'))
    File "/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-ins/Hama.bundle/Contents/Code/TheTVDBv2.py", line 170, in GetMetadata
    if season!='0': abs_number = str(int(abs_number) + 1)
    File "/usr/lib/plexmediaserver/Resources/Plug-ins-215c28d86/Framework.bundle/Contents/Resources/Versions/2/Python/Framework/code/sandbox.py", line 107, in 
    getiter = lambda x: x.iter(),
    AttributeError: 'NoneType' object has no attribute 'iter'

--- Contents/Code/TheTVDBv2.py
+++ Contents/Code/TheTVDBv2.py
@@ -83,13 +83,12 @@ def GetMetadata(media, movie, error_log, lang, metadata_source, AniDBid, TVDBid,
     else:             #JSON format: 'data': [{"seriesId", "name", "image", "lastUpdated", "imageAuthor", "role", "sortOrder", "id", "imageAdded", },...]
       Log("TheTVDB.GetMetadata() - TVDB_ACTORS_URL: {}, actor_json: {}".format(TVDB_ACTORS_URL % TVDBid, actor_json))  
       TheTVDB_dict['roles'] = []
-      if actor_json is not None:
-        for role in actor_json:
-          try:
-            SaveDict([{'role': Dict(role, 'role'), 'name': Dict(role, 'name'), 'photo': TVDB_IMG_ROOT + role['image'] if Dict(role, 'image') else ''}], TheTVDB_dict, 'roles')
-            Log.Info('TheTVDB.GetMetadata() - role: "{}", name: "{}", photo: "{}"'.format(Dict(role, 'role'), Dict(role, 'name'), Dict(role, 'image')))
-          except Exception as e:  Log.Info("TheTVDB.GetMetadata() - 'roles' - error: '{}', role: '{}'".format(str(e), str(role)))
-  
+      for role in actor_json or []:
+        try:
+          SaveDict([{'role': Dict(role, 'role'), 'name': Dict(role, 'name'), 'photo': TVDB_IMG_ROOT + role['image'] if Dict(role, 'image') else ''}], TheTVDB_dict, 'roles')
+          Log.Info('TheTVDB.GetMetadata() - role: "{}", name: "{}", photo: "{}"'.format(Dict(role, 'role'), Dict(role, 'name'), Dict(role, 'image')))
+        except Exception as e:  Log.Info("TheTVDB.GetMetadata() - 'roles' - error: '{}', role: '{}'".format(str(e), str(role)))
+
   ### TVDB Series JSON ###
   serie_json = {}
   try:     serie_json = GetResultFromNetwork(TVDB_SERIES_URL % TVDBid, additionalHeaders={'Accept-Language': lang} if lang!='en' else {})['data']
