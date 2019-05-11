6ca35ca35a5976198f271a23442d8950b0f9b01b
EndOfLine369 <dbrown369@gmail.com>
2019-05-10 22:58:59 -0500

    Load preexisting file if pull fails and 'file==None'
    Would only load a preexisting if pull was sucessful but data was seen as bad.

--- Contents/Code/common.py
+++ Contents/Code/common.py
@@ -384,15 +384,17 @@ def LoadFile(filename="", relativeDirectory="", url="", cache=CACHE_1DAY*6, head
     netLock.release()
     
     # File checks and saving as cache
-    if file:
-      if len(file)>64 or '{' in file:
-        Saved = True
-        SaveFile(filename, file, relativeDirectory)
+    if file and (len(file)>64 or '{' in file):
+      Saved = True
+      SaveFile(filename, file, relativeDirectory)
+    else:
+      Log.Info('[!] File received too small (<64 bytes), file: "{}"'.format(file))
+      file=None  #if str(file).startswith("<Element error at ") or file in ('<error>Banned</error>', '<error>aid Missing or Invalid</error>'): 
+      if Data.Exists(relativeFilename):
+        Log.Root("Loading preexisting file '{}'".format(relativeFilename))
+        file = Data.Load(relativeFilename) #present, cache expired but online version incorrect or not available
       else:
-        Log.Info('[!] File received too small (<64 bytes), file: "{}"'.format(file))
-        file=None  #if str(file).startswith("<Element error at ") or file in ('<error>Banned</error>', '<error>aid Missing or Invalid</error>'): 
-        if Data.Exists(relativeFilename):  file = Data.Load(relativeFilename) #present, cache expired but online version incorrect or not available
-        else:                              Log.Root("No preexisting file '{}' to load".format(relativeFilename))
+        Log.Root("No preexisting file '{}' to load".format(relativeFilename))
 
   if isinstance(file, basestring):
     if file.startswith('<?xml '):
