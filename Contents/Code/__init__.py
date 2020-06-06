4088872084d3ce3f376179d899299d55dd584f7f
ZeroQI <benjamin.brisson@gmail.com>
2020-06-06 11:52:25 +0100

    re-enabling MyAnimeList on atarashii API
    # Source agent:     https://github.com/Fribb/MyAnimeList.bundle/blob/master/Contents/Code/__init__.py
    # API xml exemple:  https://atarashii.fribbtastic.net/web/2.1/anime/33487

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -21,7 +21,7 @@ import FanartTV          # Functions: GetMetadata                               
 import Plex              # Functions: GetMetadata                                                Variables: None
 import TVTunes           # Functions: GetMetadata                                                Variables: None
 import OMDb              # Functions: GetMetadata                                                Variables: None
-#import MyAnimeList       # Functions: GetMetadata                                                Variables: None
+import MyAnimeList       # Functions: GetMetadata                                                Variables: None
 import Local             # Functions: GetMetadata                                                Variables: None
 import anidb34           # Functions: AdjustMapping                                              Variables: None
 
@@ -142,16 +142,16 @@ def Update(metadata, media, lang, force, movie):
   dict_Plex                                                     =        Plex.GetMetadata(metadata, error_log, TVDBid, Dict(dict_TheTVDB, 'title'))
   dict_TVTunes                                                  =     TVTunes.GetMetadata(metadata, Dict(dict_TheTVDB, 'title'), Dict(mappingList, AniDBid, 'name'))  #Sources[m:eval('dict_'+m)]
   dict_OMDb                                                     =        OMDb.GetMetadata(movie, IMDbid)  #TVDBid=='hentai'
-  #dict_MyAnimeList                                              = MyAnimeList.GetMetadata(movie, MALid )
+  dict_MyAnimeList                                              = MyAnimeList.GetMetadata(MALid, "movie" if movie else "tvshow", media)
   dict_Local                                                    =       Local.GetMetadata(media, movie)
   if anidb34.AdjustMapping(source, mappingList, dict_AniDB, dict_TheTVDB):
     dict_AniDB, ANNid, MALid                                    =       AniDB.GetMetadata(media, movie, error_log,       source, AniDBid, TVDBid, AnimeLists.AniDBMovieSets, mappingList)
   Log.Info('=== Update() ==='.ljust(157, '='))
   Log.Info("AniDBid: '{}', TVDBid: '{}', TMDbid: '{}', IMDbid: '{}', ANNid:'{}', MALid: '{}'".format(AniDBid, TVDBid, TMDbid, IMDbid, ANNid, MALid))
   common.write_logs(media, movie, error_log, source, AniDBid, TVDBid)
-  common.UpdateMeta(metadata, media, movie, {'AnimeLists': dict_AnimeLists, 'AniDB':       dict_AniDB,       'TheTVDB': dict_TheTVDB, 'TheMovieDb': dict_TheMovieDb, 
-                                             'FanartTV':   dict_FanartTV,   'tvdb4':       dict_tvdb4,       'Plex':    dict_Plex,    'TVTunes':    dict_TVTunes, 
-                                             'OMDb':       dict_OMDb,       'Local':   dict_Local}, mappingList)  #'MyAnimeList': dict_MyAnimeList, 
+  common.UpdateMeta(metadata, media, movie, {'AnimeLists': dict_AnimeLists, 'AniDB':       dict_AniDB,       'TheTVDB':     dict_TheTVDB, 'TheMovieDb': dict_TheMovieDb, 
+                                             'FanartTV':   dict_FanartTV,   'tvdb4':       dict_tvdb4,       'Plex':        dict_Plex,    'TVTunes':    dict_TVTunes, 
+                                             'OMDb':       dict_OMDb,       'Local':       dict_Local,       'MyAnimeList': dict_MyAnimeList}, mappingList) 
   Log.Info("end: {}".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S,%f")))
   Log.Close()
 
